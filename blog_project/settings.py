c543bd3399a4278a00423a35e0d224c46c16c10d
Juli <arcieri.julieta@gmail.com>
2024-08-12 22:21:21 -0300

    agregamos la imagen del home

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -145,6 +145,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
